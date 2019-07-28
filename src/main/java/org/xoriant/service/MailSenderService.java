0d34893ab60d9a4f70f29f18849e485a08cbab9f
GaneshMedage <53404106+gdmedage@users.noreply.github.com>
2019-07-28 22:15:06 +0530

    Added new project
    Added new project for sending mails with ActiveMQ.

---
+++ src/main/java/org/xoriant/service/MailSenderService.java
@@ -0,0 +1,57 @@
+package org.xoriant.service;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jms.annotation.JmsListener;
+import org.springframework.mail.MailException;
+import org.springframework.mail.SimpleMailMessage;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.scheduling.annotation.Async;
+import org.springframework.stereotype.Component;
+import org.xoriant.wrapper.MessageWrapper;
+
+@Component
+public class MailSenderService {
+
+  private JavaMailSender javaMailSender;
+
+
+  @Autowired
+  public MailSenderService(JavaMailSender javaMailSender) {
+    this.javaMailSender = javaMailSender;
+  }
+
+
+  /**
+   * This function is used to send mail without attachment.This JavaMailSender Interface is used to
+   * send Mail in Spring Boot. This JavaMailSender extends the MailSender Interface which contains
+   * send() function. SimpleMailMessage Object is required because send() function uses object of
+   * SimpleMailMessage as a Parameter
+   * 
+   * @param user
+   * @throws MailException
+   */
+  @Async
+  @JmsListener(destination = "xoriant.queue")
+  public void receiveMessage(MessageWrapper messageWrapper) {
+
+    String[] mailList = {"ganesh.medage@xoriant.com"};
+    /*
+     * This send() contains an Object of SimpleMailMessage as an Parameter
+     */
+    if (messageWrapper.getQuestionId() != 0) {
+      SimpleMailMessage mail = new SimpleMailMessage();
+      mail.setTo(mailList);
+      // mail.setFrom("");
+      mail.setSubject("Question Posted - Testing Mail API.");
+      mail.setText(messageWrapper.getQuestionText());
+      javaMailSender.send(mail);
+      System.out.println("Mail Sent");
+      try {
+        Thread.sleep(5000);
+      } catch (InterruptedException e) {
+        e.printStackTrace();
+      }
+    }
+
+  }
+}
---
+++ src/main/java/org/xoriant/wrapper/MessageWrapper.java
@@ -0,0 +1,18 @@
+package org.xoriant.wrapper;
+
+import java.io.Serializable;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Setter
+@Getter
+public class MessageWrapper implements Serializable {
+
+  /**
+   * SerialVersionUID
+   */
+  private static final long serialVersionUID = 1881085568366904200L;
+  private long questionId;
+  private String questionText;
+}
